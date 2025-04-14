8c78ffdca52f9ae5a1545ad1f49d0687acfa351e
Quinn <quinnthepigeon@gmail.com>
2025-04-14 16:20:45 +0200

    implement input and rework window implementation
    moved the window variable as a constant in the window module, and
    implement more of GLFW logic, including some basic input handling.

---
+++ src/io/input.c
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 Quinn
+// Licensed under the MIT Licence. See LICENSE for details
+#include "input.h"
+
+#include <GLFW/glfw3.h>
+
+void key_callback(GLFWwindow* win, int key, int scancode, int action, int mods) {
+	(void)win, (void)key, (void)scancode, (void)action, (void)mods; // make the compiler shut up as this is fine
+#ifndef NDEBUG
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(win, 1);
+#endif
+}
