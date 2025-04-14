09b772dfedd00e3888a8e19d22483df5d8171dec
Quinn <quinnthepigeon@gmail.com>
2025-04-14 13:25:13 +0200

    rename window/render* to io/window*
    this makes more sense, as we'll add more source files in this category

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 // Licensed under the MIT Licence. See LICENSE for details
 #include "GLFW/glfw3.h"
 #include "error.h"
-#include "window/render.h"
+#include "io/window.h"
 
 struct renderdat rdat; // contains the relevant data needed for rendering, contains rubbish data until init was
 
@@ -10,7 +10,7 @@ static inline int init(void) {
 	glfwInitHint(GLFW_JOYSTICK_HAT_BUTTONS, GLFW_FALSE); // disable joystick buttons
 
 	if (!glfwInit()) return 1;        // initialize GLFW
-	if (render_init(&rdat)) return 1; // initialize the rendering and create a window
+	if (window_init(&rdat)) return 1; // initialize the rendering and create a window
 
 	return 0;
 }
