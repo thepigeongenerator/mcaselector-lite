42646baa1a57810634b055278130a9ec988fb74d
Quinn <quinnthepigeon@proton.me>
2025-05-21 12:13:15 +0200

    don't indent post-processes directives

--- src/util/atrb.h
+++ src/util/atrb.h
@@ -14,38 +14,36 @@
 // define the attributes where possible
 #if defined(__GNUC__) || defined(__clang__)
 
-# if __has_attribute(deprecated)
-#  undef atrb_deprecated
-#  define atrb_deprecated __attribute__((deprecated))
-# endif
-
-# if __has_attribute(unused)
-#  undef atrb_unused
-#  define atrb_unused __attribute__((unused))
-# endif
-
-# if __has_attribute(pure)
-#  undef atrb_pure
-#  define atrb_pure __attribute__((pure))
-# endif
-
-
-# if __has_attribute(const)
-#  undef atrb_const
-#  define atrb_const __attribute__((const))
-# endif
-
-# if __has_attribute(format)
-#  undef atrb_format
-#  define atrb_format(...) __attribute__((format(__VA_ARGS__)))
-# endif
-
-# if __has_attribute(nonnull)
-#  undef atrb_nonnull
-#  define atrb_nonnull(...) __attribute__((nonnull(__VA_ARGS__)))
-# endif
-#elif defined(_MSC_VER)
-# undef atrb_depatrb_deprecated
-# define atrb_deprecated __declspec(deprecated)
+#if __has_attribute(deprecated)
+#undef atrb_deprecated
+#define atrb_deprecated __attribute__((deprecated))
 #endif
 
+#if __has_attribute(unused)
+#undef atrb_unused
+#define atrb_unused __attribute__((unused))
+#endif
+
+#if __has_attribute(pure)
+#undef atrb_pure
+#define atrb_pure __attribute__((pure))
+#endif
+
+#if __has_attribute(const)
+#undef atrb_const
+#define atrb_const __attribute__((const))
+#endif
+
+#if __has_attribute(format)
+#undef atrb_format
+#define atrb_format(...) __attribute__((format(__VA_ARGS__)))
+#endif
+
+#if __has_attribute(nonnull)
+#undef atrb_nonnull
+#define atrb_nonnull(...) __attribute__((nonnull(__VA_ARGS__)))
+#endif
+#elif defined(_MSC_VER)
+#undef atrb_depatrb_deprecated
+#define atrb_deprecated __declspec(deprecated)
+#endif
