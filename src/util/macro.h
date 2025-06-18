6a10122858cb388b61b65208a84f1be9d2bfb9e8
Quinn <quinnthepigeon@proton.me>
2025-06-18 13:24:04 +0200

    define some more utility functionality

---
+++ src/util/macro.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2025 Quinn
+// Licensed under the MIT Licence. See LICENSE for details
+#pragma once
+
+#define MACRO_WIDTH(t)   (sizeof(t) * 8) // gets the bit width of a type
+#define MACRO_CAT(x, y)  x##y            // concatenate two macro variables together
+#define MACRO_CAT2(x, y) MACRO_CAT(x, y) // concatenate two macro variables together
+#define MACRO_STR(v)     #v              // for converting macro variable into a string
+#define MACRO_STR2(v)    MACRO_STR(v)    // for a recursive string generation
---
+++ src/util/types.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2025 Quinn
+// Licensed under the MIT Licence. See LICENSE for details
+#pragma once
+
+/* integer types */
+typedef unsigned int uint;         // ≥16 bit unsigned integer
+typedef unsigned long ulong;       // ≥32 bit unsigned integer
+typedef signed long long llong;    // ≥64 bit signed integer
+typedef unsigned long long ullong; // ≥64 bit unsigned integer
