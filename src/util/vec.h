271773a28cdf36f83720e750e7115cb750253811
Quinn <quinnthepigeon@proton.me>
2025-07-03 11:44:59 +0200

    replace vectors with SMID vectors.

---
+++ src/util/vec.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "types.h"
+
+#if defined(__has_attribute) && __has_attribute(vector_size)
+typedef float fvec2 __attribute__((vector_size(sizeof(float) * 2))); // SMID vector for 2 `float`
+typedef float fvec4 __attribute__((vector_size(sizeof(float) * 4))); // SMID vector for 4 `float`
+
+typedef u8 u8vec2 __attribute__((vector_size(sizeof(u8) * 2))); // SMID vector for 2 `u8`
+typedef u8 u8vec4 __attribute__((vector_size(sizeof(u8) * 4))); // SMID vector for 4 `u8`
+#else
+#error the platform is unsupported, attribute vector_size must be available (and so does __has_attribute)
+#endif
+
+#define VX 0
+#define VY 1
+#define VZ 2
+#define VW 3
