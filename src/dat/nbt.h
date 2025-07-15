be87ccbe225ab81cd02db6fccc7789507b4a0f9a
Quinn <quinnthepigeon@proton.me>
2025-07-15 10:37:10 +0200

    explicitly set hexadecimal values in NBT tags

--- src/dat/nbt.h
+++ src/dat/nbt.h
@@ -13,17 +13,17 @@
 /* specifies the NBT tag IDs.
  * NOTE: every type is stored as BE (big-endian) in the file. */
 enum nbt_tagid {
-	NBT_END,      // signifies the end of a compound tag
-	NBT_I8,       // next byte is for an 8 bit signed integer.
-	NBT_I16,      // next 2 bytes are for a 16 bit signed integer
-	NBT_I32,      // next 4 bytes are for a 32 bit signed integer
-	NBT_I64,      // next 8 bytes are for a 64 bit signed integer
-	NBT_F32,      // next 4 bytes are for a single-precision floating-point
-	NBT_F64,      // next 8 bytes are for a double-precision floating-point
-	NBT_ARR_I8,   // starts with a i32, denoting size, followed by the i8 data
-	NBT_STR,      // starts with a u16, denoting size, followed by the UTF-8 data
-	NBT_LIST,     // starts with an ID, followed by a 32 bit signed integer denoting the size
-	NBT_COMPOUND, // compound tag, contains tags and is delimited by `NBT_END`
-	NBT_ARR_I32,  // starts with a i32, denoting size, followed by the i32 data
-	NBT_ARR_I64,  // starts with a i32, denoting size, followed by the u32 data
+	NBT_END = 0x00,      // signifies the end of a compound tag
+	NBT_I8 = 0x01,       // next byte is for an 8 bit signed integer.
+	NBT_I16 = 0x02,      // next 2 bytes are for a 16 bit signed integer
+	NBT_I32 = 0x03,      // next 4 bytes are for a 32 bit signed integer
+	NBT_I64 = 0x04,      // next 8 bytes are for a 64 bit signed integer
+	NBT_F32 = 0x05,      // next 4 bytes are for a single-precision floating-point
+	NBT_F64 = 0x06,      // next 8 bytes are for a double-precision floating-point
+	NBT_ARR_I8 = 0x07,   // starts with a i32, denoting size, followed by the i8 data
+	NBT_STR = 0x08,      // starts with a u16, denoting size, followed by the UTF-8 data
+	NBT_LIST = 0x09,     // starts with an ID, followed by a 32 bit signed integer denoting the size
+	NBT_COMPOUND = 0x0A, // compound tag, contains tags and is delimited by `NBT_END`
+	NBT_ARR_I32 = 0x0B,  // starts with a i32, denoting size, followed by the i32 data
+	NBT_ARR_I64 = 0x0C,  // starts with a i32, denoting size, followed by the u32 data
 };
