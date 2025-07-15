db76d6992bc9ff8957b61b5b9aa2b6c75b626798
Quinn <quinnthepigeon@proton.me>
2025-07-15 13:01:34 +0200

    add function for comparing an NBT string with a C string

--- src/dat/nbt.c
+++ src/dat/nbt.c
@@ -2,12 +2,26 @@
 
 #include <endian.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "../util/types.h"
 
 /* returns the string length from a specific location in the buffer */
 static inline u16 nbt_strlen(u8 const *restrict buf) {
 	return be16toh(*(u16 *)(buf));
+}
+
+/* compares the string in `buf` to `matstr`.
+ * returns `=0` if equal, `>0` if buf is greater, `<0` if matstr is greater. */
+static int nbt_cmpstr(char const *restrict matstr, u8 const *restrict buf) {
+	u16 len = nbt_strlen(buf);
+
+	// allocate and copy bytes
+	char str[len + 1];
+	memcpy(str, buf + 2, len);
+	str[len] = '\0';
+
+	return strncmp(str, matstr, len);
 }
 
 int nbt_proc(void **restrict datout, u8 const *restrict buf, size_t len) {
