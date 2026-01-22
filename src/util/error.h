102f9295fd78ebe8a75664a0154f921da59a819b
Quinn <quinnthepigeon@proton.me>
2026-01-22 12:23:30 +0100

    Delete __VA_OPT__ from logging macros, since this is unnecessary.

--- src/util/error.h
+++ src/util/error.h
@@ -16,8 +16,8 @@ void error_warn(uint ln, const char *restrict file, const char *restrict fmt, ..
 void error_error(uint ln, const char *restrict file, const char *restrict fmt, ...);
 void error_fatal(uint ln, const char *restrict file, const char *restrict fmt, ...) NORET;
 
-#define debug(...) error_debug(__LINE__, __FILE__ __VA_OPT__(, ) __VA_ARGS__)
-#define info(...)  error_info(__LINE__, __FILE__ __VA_OPT__(, ) __VA_ARGS__)
-#define warn(...)  error_warn(__LINE__, __FILE__ __VA_OPT__(, ) __VA_ARGS__)
-#define error(...) error_error(__LINE__, __FILE__ __VA_OPT__(, ) __VA_ARGS__)
-#define fatal(...) error_fatal(__LINE__, __FILE__ __VA_OPT__(, ) __VA_ARGS__)
+#define debug(...) error_debug(__LINE__, __FILE__, __VA_ARGS__)
+#define info(...)  error_info(__LINE__, __FILE__, __VA_ARGS__)
+#define warn(...)  error_warn(__LINE__, __FILE__, __VA_ARGS__)
+#define error(...) error_error(__LINE__, __FILE__, __VA_ARGS__)
+#define fatal(...) error_fatal(__LINE__, __FILE__, __VA_ARGS__)
