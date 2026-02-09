ef3349474530b4aca6b8f26558f65bfa6a505541
Quinn <quinn@thepigeongenerator.xyz>
2026-02-09 11:11:14 +0100

    Clean up Makefile, move Windows logic into a seperate file.
    - Removed stripped binary creation, since we really do not use it.
    - Moved created binary files to just create in the project root.
    - Merged check rules
    - Moved msg into the else of quiet, so it functions as a switch

---
+++ Windows_NT.mk
@@ -0,0 +1,14 @@
+# Copyright (C)2026 MCA-Selector-Lite
+# Licensed under GPL-2.0-only. For further information,
+# view `git log`, and the COPYING and CONTRIBUTORS files
+# at www.github.com/thepigeongenerator/mcaselector-lite.
+#
+# This file is specifically for Windows_NT systems.
+# It is included in Makefile if Windows_NT is detected.
+$(warning Detected  Windows_NT, please refer to the documentation if you encounter issues.)
+
+# BUG: I am purposefully neglecting this at the moment.
+
+# Append .exe to the name to match the PE format.
+NAME := $(NAME).exe
+LDLIBS += -lopengl32 -lgdi32
