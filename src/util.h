27edc3f8a7b864efee26c274e423d486aacec0f4
Quinn <quinn@thepigeongenerator.xyz>
2026-03-04 14:04:35 +0100

    Define macro which calls __builtin_unreachable, or traps when error.

--- src/util.h
+++ src/util.h
@@ -4,7 +4,14 @@
  * at www.github.com/thepigeongenerator/mcxedit. */
 #ifndef MCXEDIT_UTIL_H
 #define MCXEDIT_UTIL_H 1
+#include "err.h"
 
 extern const char *argv0;
+
+#ifdef NDEBUG
+#define UNREACHABLE() __builtin_unreachable()
+#else
+#define UNREACHABLE() errx(-5, "Reached unreachable point at %s:%d!", __FILE__, __LINE__)
+#endif
 
 #endif /* MCXEDIT_UTIL_H */
