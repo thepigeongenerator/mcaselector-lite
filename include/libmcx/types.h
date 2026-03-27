aa0f0d8323da159e5b0ba900fc4d89448e25983f
Quinn Zieltjens <quinnthepigeon@proton.me>
2026-03-27 12:49:32 +0100

    Overhaul ``types.h`` to be a bit more standard-compliant by using ``stdint.h``.

--- include/libmcx/types.h
+++ include/libmcx/types.h
@@ -5,13 +5,18 @@
 #ifndef MCXEDIT_TYPES_H
 #define MCXEDIT_TYPES_H 1
 
+#include <stddef.h>
+#include <stdint.h>
+
+/* __CHECKER__ is defined by sparse, here we use it to define some
+ * sparse-specific attributes we'll be using in this header. */
 #ifdef __CHECKER__
 #define BITWISE __attribute__((bitwise))
 #define FORCE   __attribute__((force))
 #else
 #define BITWISE
 #define FORCE
-#endif
+#endif /* __CHECKER__ */
 
 typedef signed char        schar;
 typedef long long          llong;
@@ -21,26 +26,14 @@ typedef unsigned int       uint;
 typedef unsigned long      ulong;
 typedef unsigned long long ullong;
 
-/* TODO: Convert these into their standard variations. */
-#if __SIZEOF_SIZE_T__ == __SIZEOF_INT__
-typedef uint size_t;
-typedef int  ssize_t;
-#elif __SIZEOF_SIZE_T__ == __SIZEOF_LONG__
-typedef ulong size_t;
-typedef long  ssize_t;
-#elif __SIZEOF_SIZE_T__ == __SIZEOF_LONG_LONG__
-typedef ullong size_t;
-typedef llong  ssize_t;
-#endif
-
-typedef __INT8_TYPE__   __s8;
-typedef __INT16_TYPE__  __s16;
-typedef __INT32_TYPE__  __s32;
-typedef __INT64_TYPE__  __s64;
-typedef __UINT8_TYPE__  __u8;
-typedef __UINT16_TYPE__ __u16;
-typedef __UINT32_TYPE__ __u32;
-typedef __UINT64_TYPE__ __u64;
+typedef int8_t   __s8;
+typedef int16_t  __s16;
+typedef int32_t  __s32;
+typedef int64_t  __s64;
+typedef uint8_t  __u8;
+typedef uint16_t __u16;
+typedef uint32_t __u32;
+typedef uint64_t __u64;
 
 typedef __u16 BITWISE __be16;
 typedef __u32 BITWISE __be32;
@@ -48,13 +41,6 @@ typedef __u64 BITWISE __be64;
 typedef __u16 BITWISE __le16;
 typedef __u32 BITWISE __le32;
 typedef __u64 BITWISE __le64;
-
-#if __SIZEOF_FLOAT__ == 4
-typedef float __f32;
-#endif
-#if __SIZEOF_DOUBLE__ == 8
-typedef double __f64;
-#endif
 
 #ifdef MCXEDIT_SOURCE
 typedef __s8   s8;
@@ -71,8 +57,13 @@ typedef __be64 be64;
 typedef __le16 le16;
 typedef __le32 le32;
 typedef __le64 le64;
-typedef __f32  f32;
-typedef __f64  f64;
+
+#if __SIZEOF_FLOAT__ == 4
+typedef float f32;
 #endif
+#if __SIZEOF_DOUBLE__ == 8
+typedef double f64;
+#endif
+#endif /* MCXEDIT_SOURCE */
 
 #endif /* MCXEDIT_TYPES_H */
